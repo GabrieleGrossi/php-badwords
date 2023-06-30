86c4bd2e998be5132a13f886c65183215f5d0280
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-30 15:12:20 +0200

    Create two files and start to create the text area

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h1>
+        Php Badwords
+    </h1>
+    <form action="./server.php" type="GET">
+        <textarea name="text" id="text" cols="30" rows="10" placeholder="Insert the text"></textarea>
+        
+    </form>
+</body>
+</html>
